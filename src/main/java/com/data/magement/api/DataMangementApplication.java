8d7e7b62463def5f658166fc642d056abc2206a7
Vishnu Vicki <98948687+Vishnu-ADS@users.noreply.github.com>
2023-06-20 17:47:41 +0530

    initial project

---
+++ src/main/java/com/data/magement/api/DataMangementApplication.java
@@ -0,0 +1,13 @@
+package com.data.magement.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMangementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data/magement/api/DataMangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.magement.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMangementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
